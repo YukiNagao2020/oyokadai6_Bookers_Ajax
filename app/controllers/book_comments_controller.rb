08cd9bbd6b62b123dafe7e4aaf998a11d9ee04f0
YukiNagao2020 <yuki.n0301@gmail.com>
2021-08-02 05:41:52 +0000

    oyokadai6 Bookers2 Ajax

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,29 @@
+class BookCommentsController < ApplicationController
+    before_action :authenticate_user!
+    
+	def index
+	  	@book_comments = BookComment.all
+	  	@book_comment = BookComment.new(book_comment_params)
+	end	
+
+	def create
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		@book_comment.save
+  	
+	end
+
+	def destroy
+		@book = Book.find(params[:book_id])
+  	book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+		
+	end
+
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+    devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+  end
+    
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+    
+  get '/search' => 'searches#search'
+  
+end
